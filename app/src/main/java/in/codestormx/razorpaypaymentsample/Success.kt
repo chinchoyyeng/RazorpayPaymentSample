cd775233ee5b057840eae96eb408301bc338b654
Surya <suryavmds7000@codestormx.in>
2020-11-27 20:14:33 +0530

    Initial commit

---
+++ app/src/main/java/in/codestormx/razorpaypaymentsample/Success.kt
@@ -0,0 +1,11 @@
+package `in`.codestormx.razorpaypaymentsample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Success : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_success)
+    }
+}
